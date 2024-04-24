23a9d6c5902b4ef3ff28bffa80ec0d01eeb76130
tarribet <monsieur.arribet28@gmail.com>
2024-04-24 12:16:14 +0200

    Add database table creation and print server running message

--- server.go
+++ server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 // launch a server on port 8080 and display auth.html
 func server() {
 	http.Handle("/", http.FileServer(http.Dir("./")))
-	http.ListenAndServe("localhost:8080", nil)
+	fmt.Println("Server is running on port 8080.")
+	http.ListenAndServe("0.0.0.0:8080", nil)
 }
